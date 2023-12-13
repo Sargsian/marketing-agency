df1312b226b4c745a5014bba8badb9e8626a69c1
Gor Sargsian <sargsian@Gors-MacBook-Pro.local>
2023-12-12 18:11:04 -0800

    added company address, fixed overflowing issue

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -20,7 +20,7 @@ const Policy = () => {
 
   console.log(title);
   return (
-    <>
+    <div className="w-full relative overflow-hidden">
       <Head>
         <title>{`Boost - ${title}`}</title>
         <meta name="title" content="Page doesnt exist" />
@@ -31,8 +31,8 @@ const Policy = () => {
       <Image
         priority
         src={"/assets/images/404.png"}
-        alt="404"
-        width={1920}
+        alt="privacy policy"
+        width={1550}
         className="absolute origin-top scale-[150%] md:left-[20%] md:top-0 md:scale-100"
         height={100}
       />
@@ -40,9 +40,9 @@ const Policy = () => {
         <h1 className="max-w-[600px] text-[32px] font-semibold leading-none tracking-tighter drop-shadow-lg sm:text-6xl md:text-7xl md:leading-[96px] xl:text-[96px]">
           {t("title")}
         </h1>
-        <div className="mt-[58px] md:mt-[100px] flex max-w-[470px] flex-col gap-[58px] md:gap-[93px] drop-shadow-lg md:drop-shadow-none">
+        <div className="mt-[58px] flex max-w-[470px] flex-col gap-[58px] drop-shadow-lg md:mt-[100px] md:gap-[93px] md:drop-shadow-none">
           <div>
-            <h3 className="mb-8 md:mb-[37px] uppercase heading-5">
+            <h3 className="mb-8 uppercase heading-5 md:mb-[37px]">
               {t("protectionHeader")}
             </h3>
             <p className="whitespace-pre-line opacity-60 drop-shadow-md lead-1 md:drop-shadow-none">
@@ -50,7 +50,7 @@ const Policy = () => {
             </p>
           </div>
           <div id="terms">
-            <h3 className="mb-8 md:mb-[37px] uppercase heading-5">
+            <h3 className="mb-8 uppercase heading-5 md:mb-[37px]">
               {t("cookieHeader")}
             </h3>
             <p className="whitespace-pre-line opacity-60 lead-1">
@@ -66,16 +66,25 @@ const Policy = () => {
             <p className="opacity-60 lead-1"> {t("cookieText2")}</p>
           </div>
           <div>
-            <h3 className="mb-8 md:mb-[37px] uppercase heading-5">
+            <h3 className="mb-8 uppercase heading-5 md:mb-[37px]">
               {t("confidentialInfoHeader")}
             </h3>
             <p className="opacity-60 lead-1">{t("confidentialInfoText")}</p>
+          </div>
+          <div>
+            <p>ADS BOOST LTD</p>
+            <p>
+              Address: Prodromou, 75, Oneworld Parkview House, 4th floor, 2063,
+              Nicosia
+            </p>
+            <p>VAT No.: 10431912X</p>
+            <p>REG number: HE 431912</p>
           </div>
           <h2 className="uppercase heading-4">{t("appreciation")}</h2>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
